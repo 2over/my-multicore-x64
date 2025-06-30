c1b093c781add07903fed58cd9becfbce5e411ad
vinpink <mysunshine2126@gmail.com>
2025-06-29 18:48:00 -0700

    feat: 进入保护模式，支持C语言开发与调试

--- oskernel/boot/boot.asm
+++ oskernel/boot/boot.asm
@@ -6,52 +6,112 @@
 
 [ORG 0x7c00]
 
-%include "oskernel/include/global.inc.cover"
+[SECTION .data]
+BOOT_MAIN_ADDR equ 0x500
 
 [SECTION .text]
 [BITS 16]
 
-global _start
-_start:
+global boot_start
+boot_start:
     ; 设置屏幕模式为文本模式，清除屏幕
     mov ax, 3
     int 0x10
 
-    ; 通过BIOS中断读硬盘，将setup读入内存
-    push dword 0                        ; 向栈中压入4B, 起始扇区的高4B
-    push dword SETUP_SECTOR_START       ; 向栈中压入4B, 起始扇区的低4B
-    push word SETUP_ADDR_BASE >> 4      ; 读入内存的位置(逻辑地址)(为什么右移4位？ 0x13终端历程要求)
-    push word 0                         ; 读入内存的位置(段内偏移)
-    push word SETUP_SECTOR_COUNT        ; 读多少扇区
-    push word 0x10                      ; 读硬盘的BIOS例程需要传入的地址结构的大小
-    mov si, sp
-    mov ah, 0x42                        ; 功能号(0x42表示读硬盘)
-    mov dl, 0x80                        ; 读哪快盘(0x80为第一块盘)
-
-    ; 中断 0x13（即 INT 13h）是 BIOS 提供的磁盘服务中断，它是 实模式下访问硬盘、软盘等存储设备的主要方式，尤其在操作系统引导阶段（如 bootloader）非常常见。
-    int 0x13                            ; 读硬盘BIOS中断(成功AH=0, CF=0, 失败CF=1, AH=错误码)
-    jc  .read_hd_fail
-
-    add sp, 0x10
+    ; 将setup读入内存
+    mov edi, BOOT_MAIN_ADDR ; 读到哪里
+    mov ecx, 1              ; 从哪个扇区开始读
+    mov bl, 2               ; 读多少扇区
+    call read_hd
 
     ; 跳过去
     mov     si, jmp_to_setup
     call    print
 
-    xchg bx, bx
 
-    jmp     SETUP_ADDR_BASE
+    jmp     BOOT_MAIN_ADDR
 
+read_hd:
+    ; 0x1f2 8bit 指定读取或写入的扇区数
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
 
-.read_hd_fail:
-    mov si, read_disk_error_msg
-    call print
+    ; 0x1f3 8bit LBA地址的低8位 0-7
+    inc dx
+    mov al, cl
+    out dx, al
 
-    jmp stop_cpu
+    ; 0x1f4 8bit LBA地址的中8位 8-15
+    inc dx
+    mov al, ch
+    out dx, al
 
-stop_cpu:
-    hlt
-    jmp stop_cpu
+    ; 0x1f5 8bit LBA地址的高八位 16-23
+    inc dx
+    shr ecx, 16
+    mov al, cl
+    out dx, al
+
+    ; 0x1f6 8bit
+    ; 0-3: LBA地址的24-27
+    ; 4:   0 表示主盘 1 表示从盘
+    ; 5、7 位固定为1
+    ; 6 0表示CHS模式， 1表示LBA模式
+    inc dx
+    shr ecx, 8
+    and cl, 0b1111
+    mov al, 0b1110_0000 ; LBA模式
+    or al, cl
+    out dx, al
+
+    ; 0x1f7 8bit 命令或状态端口
+    inc dx
+    mov al, 0x20
+    out dx, al
+
+    ; 设置loop次数，读多少个扇区要loop多少次
+    mov cl, bl
+
+.start_read:
+    push cx     ; 保存loop次数, 防止被下面的代码修改破坏
+
+    call .wait_hd_prepare
+    call read_hd_data
+
+    pop cx      ; 恢复loop次数
+
+    loop .start_read
+
+.return:
+    ret
+
+; 一直等待，直到硬盘的状态是: 不繁忙，数据已准备好
+; 即第7位为0, 第3位为1, 第0位为0
+.wait_hd_prepare:
+    mov dx, 0x1f7
+
+.check:
+    in al, dx
+    and al, 0b1000_1000
+    cmp al, 0b0000_1000
+    jnz .check
+
+    ret
+
+;  读硬盘, 一次读两个字节，读256次，刚好读一个扇区
+read_hd_data:
+    mov dx, 0x1f0
+    mov cx, 256
+
+.read_word:
+    in ax, dx
+    mov [edi], ax
+    add edi, 2
+    loop .read_word
+
+    ret
+
 ; 如何调用
 ; mov si, msg   ; 1 传入字符串
 ; call print    ; 2 调用
@@ -80,8 +140,6 @@ print:
 ;
 ;0
 ;表示字符串的结尾（NULL 字符），用于标记 C 风格字符串的终止符。
-
-read_disk_error_msg: db "read disk error!", 10, 13, 0
 
 jmp_to_setup: db "jump to setup...", 10, 13, 0
 
