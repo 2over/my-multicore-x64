d15a31461ab1b5746f89c7aacee1408e78481fa3
vinpink <mysunshine2126@gmail.com>
2025-07-02 05:26:57 -0700

    feat: 测试阻断NMI与除零异常

--- x64kernel/init/main64.c
+++ x64kernel/init/main64.c
@@ -3,6 +3,7 @@
 #include "../include/kernel.h"
 #include "../include/mm.h"
 #include "../include/idt.h"
+#include "../include/io.h"
 
 
 void kernel64_main(void) {
@@ -11,8 +12,13 @@ void kernel64_main(void) {
     phy_memory_init();
     idt_init();
 
+    uchar v = in_byte(0x70);
+    v |= 0x80;
+    out_byte(0x70, v);
 
     int i = 10 /0;
+
+    printk("here\n");
     while (true) {
         asm volatile("sti;");
         asm volatile("xchg %bx, %bx; hlt;");
